90d39c5a04a0f4a93354ab510a82c094f8125ce4
sztuk <260875@edu.p.lodz.pl>
2025-10-23 08:41:06 +0200

    zmiany dodatkowe w 03

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int Policz (int a, int b) {
-    return a * b;
+    return (a + b)*(a-b);
 }
 
 int main() {
@@ -10,7 +10,7 @@ int main() {
 
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
-    std::cout << "W pierwszym branchu; 2*3=" << Policz(2, 3) << std::endl;
+    std::cout << "W pierwszym branchu; 2+3=" << Policz(2, 3) << std::endl;
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
